205bffc6971279befda9e29cfeb35ace5d452869
ripellizzari <ripellizzari@gmail.com>
2021-08-22 20:23:21 -0300

    AñadirCarrito-condicional

---
+++ Proyectos/candy-place/src/App.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+import './style.css';
+import './components/CartWidget'
+
+
+
+import './components/NavBar';
+import NavBar from './components/NavBar';
+import CardComponent from './components/CardComponent/CardComponent';
+
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <NavBar />
+        <div className="card-container">
+          <CardComponent
+            id='1'
+            name='Dientes de Dracula'
+            img='https://http2.mlstatic.com/D_NQ_NP_853655-MLA40543347879_012020-O.jpg'
+            stock='500'
+          />
+          <CardComponent
+            id='2'
+            name='Malvavisco'
+            img='https://d3ugyf2ht6aenh.cloudfront.net/stores/135/794/products/15-24e303fd9e871ba52115956928313486-640-0.jpg'
+            stock='450'
+          />
+          <CardComponent
+            id='3'
+            name='Gomitas'
+            img='https://http2.mlstatic.com/D_NQ_NP_785293-MLA41431782365_042020-O.jpg'
+            stock='750'
+          />
+          <CardComponent
+            id='4'
+            name='Anillitos de gomitas'
+            img='https://riobgdistribuciones.com/wp-content/uploads/2020/04/mogul-anillos-unidades.jpg'
+            stock='600'
+          />
+        </div>
+
+
+      </div >
+    );
+  }
+}
+
+
+
+
+export default App;
---
+++ Proyectos/candy-place/src/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+
+
+//Semantic-UI 
+import 'semantic-ui-css/semantic.min.css';
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
+
+// const items = [
+//     { nombre: "Alfajores", precio: "$300" },
+//     { nombre: "Chocolates", precio: "$500" },
+//     { nombre: "Tortas", precio: "$1200" },
+//     { nombre: "Cereales", precio: "$750" },
+// ]
+
+// const names = items.map(function (item) {
+//     return item.precio;
+// });
+
+// console.log(names);
