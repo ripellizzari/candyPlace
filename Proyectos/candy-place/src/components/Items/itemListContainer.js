593e4d2ad89080ff75506f3c0548e140850bed2e
ripellizzari <ripellizzari@gmail.com>
2021-08-29 17:48:40 -0300

    Mapeo y renderizado diferido

--- Proyectos/candy-place/src/components/Items/itemListContainer.js
+++ Proyectos/candy-place/src/components/Items/itemListContainer.js
@@ -3,18 +3,27 @@ import ItemList from "../Items/ItemList";
 
 const ItemListContainer = () => {
     const [products, setProducts] = useState([]);
-    useEffect(() => {
-        fetch('https://fakestoreapi.com/products?limit=5')
-            .then((response) => {
-                console.log(response);
-                return response.json();
 
-            })
-            .then((data) => {
-                setProducts(data);
-            });
+
+    useEffect(() => {
+        setTimeout(() => {
+            fetch('https://fakestoreapi.com//products?limit=5')
+                .then((response) => {
+                    console.log(response);
+
+                    return response.json();
+
+                })
+                .then((data) => {
+                    setProducts(data);
+                });
+        }, 2000)
+
     }, []);
+
     return <ItemList products={products} />;
+
+
 };
 
 export default ItemListContainer;
